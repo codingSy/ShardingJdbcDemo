57024a93a207b9dc780e0f2b3aa3264a937a9a2a
shaoying <Sy920819@*#>
2020-10-15 17:11:24 +0800

    sharding接入dubbo

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -5,34 +5,34 @@ CREATE DATABASE user0;
 CREATE DATABASE user1;
 
 CREATE TABLE `t_user0` (
-  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   `name` varchar(30) DEFAULT NULL COMMENT '名称',
   `sex` tinyint(2) DEFAULT NULL COMMENT '性别 0-男，1-女',
   `phone` varchar(15) DEFAULT NULL COMMENT '电话',
   `email` varchar(30) DEFAULT NULL COMMENT '邮箱',
   `password` varchar(20) DEFAULT NULL COMMENT '密码',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `t_user1` (
-  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   `name` varchar(30) DEFAULT NULL COMMENT '名称',
   `sex` tinyint(2) DEFAULT NULL COMMENT '性别 0-男，1-女',
   `phone` varchar(15) DEFAULT NULL COMMENT '电话',
   `email` varchar(30) DEFAULT NULL COMMENT '邮箱',
   `password` varchar(20) DEFAULT NULL COMMENT '密码',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `t_user2` (
-  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
   `name` varchar(30) DEFAULT NULL COMMENT '名称',
   `sex` tinyint(2) DEFAULT NULL COMMENT '性别 0-男，1-女',
   `phone` varchar(15) DEFAULT NULL COMMENT '电话',
   `email` varchar(30) DEFAULT NULL COMMENT '邮箱',
   `password` varchar(20) DEFAULT NULL COMMENT '密码',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
